07c3fb4bfd38251335fb757fb2543a3a7b93c165
Roman Sharkov <roman.scharkov@gmail.com>
2026-07-28 21:17:12 +0200

    fix: Add lang attribute to html element

--- template_templ.go
+++ template_templ.go
@@ -40,7 +40,7 @@ func templatePage(title string, sections []Section) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><head><title>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><title>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
